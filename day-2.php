ddb3c5081837f868723492cd8812ddc8ac89fa66
Julien SCHMITT <jschmitt95@protonmail.com>
2024-12-02 13:53:40 +0100

    Day 2

---
+++ day-2.php
@@ -0,0 +1,64 @@
+<?php
+
+// load the input file
+$input = file_get_contents('./inputs/inputs-2.txt');
+$lines = explode("\n", $input);
+
+// Part 1
+$safe1 = 0;
+foreach ($lines as $line) {
+    $list = explode(' ', $line);
+
+    $head = (int)$list[0];
+    $order = null;
+    for ($i = 1; $i < count($list); $i++) {
+        $current = (int)$list[$i];
+        if (!$order) {
+            $order = $current > $head ? 'asc' : 'desc';
+        } elseif ($order === 'asc' && $current < $head || $order === 'desc' && $current > $head) {
+            continue 2;
+        }
+
+        if (abs($head - $current) < 1 || abs($head - $current) > 3) {
+            continue 2;
+        }
+
+        $head = $current;
+    }
+    ++$safe1;
+}
+
+echo $safe1 . "\n";
+
+// Part 2
+$safe2 = 0;
+foreach ($lines as $line) {
+    $list = explode(' ', $line);
+
+    $head = (int)$list[0];
+    $order = null;
+    $alreadyFirstError = false;
+    for ($i = 1; $i < count($list); $i++) {
+        $current = (int)$list[$i];
+        if (!$order) {
+            $order = $current > $head ? 'asc' : 'desc';
+        } elseif ($order === 'asc' && $current < $head || $order === 'desc' && $current > $head) {
+            if ($alreadyFirstError) {
+                continue 2;
+            }
+            $alreadyFirstError = true;
+        }
+
+        if (abs($head - $current) < 1 || abs($head - $current) > 3) {
+            if ($alreadyFirstError) {
+                continue 2;
+            }
+            $alreadyFirstError = true;
+        }
+
+        $head = $current;
+    }
+    ++$safe2;
+}
+
+echo $safe2 . "\n";
